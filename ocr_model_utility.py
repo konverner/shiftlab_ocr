5d85aa273a68f367c6cb625c03430b6e9477bc07
Constantin Werner <52051213+constantin50@users.noreply.github.com>
2021-08-23 01:24:58 +0700

    Update ocr_model_utility.py

--- ocr_model_utility.py
+++ ocr_model_utility.py
@@ -1,19 +1,18 @@
-import cv2
-import editdistance
-import torch
+import io, cv2, os, torch
 import numpy as np
+from os.path import join
+import random
 
-# RESIZE AND NORMALIZE A IMAGE
+# RESIZE AND NORMALIZE IMAGE
 def process_image(img):
-    """
+    '''
     params:
     ---
     img : np.array
-
     returns
     ---
     img : np.array
-    """
+    '''
     w, h, _ = img.shape
     new_w = 64
     new_h = int(h * (new_w / w))
@@ -32,38 +31,10 @@ def process_image(img):
 
     return img
 
-# LOAD A STATE OF MODEL FROM CHK_PATH
-# IF CHK_PATH IS EMPTY THEN IT INITIALIZES THE STATE TO ZERO
-def load_from_checkpoint(model,chk_path):
-    """
-    params
-    ---
-    model : nn.Module
-
-    chk_path : string
-        path to the .pt file
-
-    returns
-    ---
-    model : nn.Module
-
-    """
-    if chk_path:
-        if torch.cuda.is_available():
-          ckpt = torch.load(chk_path)
-        else:
-          ckpt = torch.load(chk_path,  map_location=torch.device('cpu'))
-        if 'model' in ckpt:
-            model.load_state_dict(ckpt['model'])
-        else:
-            model.load_state_dict(ckpt)
-        print('weights have been loaded')
-    return model
-
 # TRANSLATE INDICIES TO TEXT
 def labels_to_text(s, idx2char):
-    """
-    params
+    '''
+    paramters
     ---
     idx2char : dict
         keys : int
@@ -73,29 +44,9 @@ def labels_to_text(s, idx2char):
     returns
     ---
     S : str
-    """
+    '''
     S = "".join([idx2char[i] for i in s])
     if S.find('EOS') == -1:
         return S
     else:
         return S[:S.find('EOS')]
-
-
-# COMPUTE CHARACTER ERROR RATE
-def char_error_rate(p_seq1, p_seq2):
-    """
-    params
-    ---
-    p_seq1 : str
-    p_seq2 : str
-
-    returns
-    ---
-    cer : float
-    """
-    p_vocab = set(p_seq1 + p_seq2)
-    p2c = dict(zip(p_vocab, range(len(p_vocab))))
-    c_seq1 = [chr(p2c[p]) for p in p_seq1]
-    c_seq2 = [chr(p2c[p]) for p in p_seq2]
-    return editdistance.eval(''.join(c_seq1),
-                             ''.join(c_seq2)) / max(len(c_seq1), len(c_seq2))
